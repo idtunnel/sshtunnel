e876375f43c4e0f4a6df3633dc1b3f43691767a6
ID Tunnel <65126630+idtunnel@users.noreply.github.com>
2020-05-20 08:53:50 +0700

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,26 +1,43 @@
 #squid
-iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
-iptables -A INPUT -p tcp --dport 3128 -j ACCEPT
-iptables -A INPUT -p tcp --dport 4343 -j ACCEPT
-iptables -A INPUT -p tcp --dport 9090 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 8888 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 8888 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 3128 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 3128 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 4343 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 4343 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 9090 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 9090 -j ACCEPT
 
 #stunnel
-iptables -A INPUT -p tcp --dport 777 -j ACCEPT
-iptables -A INPUT -p tcp --dport 444 -j ACCEPT
-iptables -A INPUT -p tcp --dport 222 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 777 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 777 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 444 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 444 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 222 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 222 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 443 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 443 -j ACCEPT
 
 #ssh
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp --dport 143 -j ACCEPT
-iptables -A INPUT -p tcp --dport 44 -j ACCEPT
-iptables -A INPUT -p tcp --dport 77 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 22 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 143 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 143 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 44 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 44 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 77 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 77 -j ACCEPT
 
 #lain nya
-iptables -A INPUT -p tcp --dport 10000 -j ACCEPT
-iptables -A INPUT -p tcp --dport 9000 -j ACCEPT
-iptables -A INPUT -p tcp --dport 81 -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 9000 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 9000 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 10000 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 10000 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 81 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 81 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p udp --dport 80 -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -p tcp --dport 80 -j ACCEPT
+
 
 #simpan iptunnel
 service iptables save
