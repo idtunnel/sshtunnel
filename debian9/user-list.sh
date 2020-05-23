3780dafad87b97821f0327d6b5dce96177aa17ce
ID Tunnel <65126630+idtunnel@users.noreply.github.com>
2020-05-23 15:12:04 +0700

    Update user-list.sh

--- debian9/user-list.sh
+++ debian9/user-list.sh
@@ -15,5 +15,5 @@ JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)
 echo "-------------------------------"
 echo "Jumlah akun: $JUMLAH user"
 echo "-------------------------------"
-echo -e "Mod by partner white-vps"
+echo -e "Mod by partner HideSSH"
 echo -e ""
