6308d75aa61dc0942b4153b715cc35d510cc2d39
ID Tunnel <65126630+idtunnel@users.noreply.github.com>
2020-06-27 10:03:07 +0700

    Create ovh-ssh.sh

---
+++ debian9/ovh-ssh.sh
@@ -0,0 +1,315 @@
+#!/bin/bash
+#created : OVH HideSSH
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+
+# detail nama perusahaan
+country=ID
+state=Semarang
+locality=JawaTengah
+organization=hidessh
+organizationalunit=HideSSH
+commonname=hidessh.com
+email=admin@hidessh.com
+
+# go to root
+cd
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# set time GMT +7 jakarta
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
+# Edit file /etc/systemd/system/rc-local.service
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+END
+
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+echo "=================  install neofetch  ===================="
+echo "========================================================="
+# install neofetch
+apt-get update -y
+apt-get -y install gcc
+apt-get -y install make
+apt-get -y install cmake
+apt-get -y install git
+apt-get -y install screen
+apt-get -y install unzip
+apt-get -y install curl
+git clone https://github.com/dylanaraps/neofetch
+cd neofetch
+make install
+make PREFIX=/usr/local install
+make PREFIX=/boot/home/config/non-packaged install
+make -i install
+apt-get -y install neofetch
+cd
+echo "clear" >> .bashrc
+echo "neofetch" >> .bashrc
+
+# instal php5.6 ubuntu 16.04 64bit
+apt-get -y update
+
+# setting port ssh
+cd
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
+echo "================  install Dropbear ======================"
+echo "========================================================="
+
+# install dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=44/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 77 "/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+
+echo "=================  install Squid3  ======================"
+echo "========================================================="
+
+# setting dan install vnstat debian 9 64bit
+apt-get -y install vnstat
+systemctl start vnstat
+systemctl enable vnstat
+chkconfig vnstat on
+chown -R vnstat:vnstat /var/lib/vnstat
+
+# install squid3
+cd
+apt-get -y install squid3
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/squid3.conf"
+sed -i $MYIP2 /etc/squid/squid.conf;
+/etc/init.d/squid restart
+
+echo "=================  install stunnel  ====================="
+echo "========================================================="
+
+# install stunnel
+apt-get install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+[dropbear]
+accept = 443
+connect = 127.0.0.1:143
+[dropbear]
+accept = 222
+connect = 127.0.0.1:22
+[dropbear]
+accept = 444
+connect = 127.0.0.1:44
+[dropbear]
+accept = 777
+connect = 127.0.0.1:77
+
+END
+
+echo "=================  membuat Sertifikat OpenSSL ======================"
+echo "========================================================="
+#membuat sertifikat
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+# konfigurasi stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/etc/init.d/stunnel4 restart
+
+# common password debian 
+wget -O /etc/pam.d/common-password "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/common-password-deb9"
+chmod +x /etc/pam.d/common-password
+
+
+echo "=================  Install badVPn (VC and Game) ======================"
+echo "========================================================="
+
+cd
+wget -O /usr/bin/badvpn-udpgw "https://github.com/idtunnel/UDPGW-SSH/raw/master/badvpn-udpgw64"
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500 --max-connections-for-client 20' /etc/rc.local
+#permission
+chmod +x /usr/bin/badvpn-udpgw
+chomod +x /etc/rc.d/rc.local
+
+#badvpn 7300
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500 --max-connections-for-client 20
+
+
+# Custom Banner SSH
+wget -O /etc/issue.net "https://github.com/idtunnel/sshtunnel/raw/master/debian9/banner-custom.conf"
+chmod +x /etc/issue.net
+
+echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
+echo "DROPBEAR_BANNER="/etc/issue.net"" >> /etc/default/dropbear
+
+# install fail2ban
+apt-get -y install fail2ban
+service fail2ban restart
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
+# download script
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/user-login.sh"
+wget -O member "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/user-list.sh"
+wget -O jurus69 "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/info.sh"
+wget -O about "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/about.sh"
+wget -O delete "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/delete.sh"
+
+echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
+
+chmod +x menu
+chmod +x usernew
+chmod +x trial
+chmod +x hapus
+chmod +x cek
+chmod +x member
+chmod +x jurus69
+chmod +x speedtest
+chmod +x info
+chmod +x about
+chmod +x delete
+
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/stunnel4 restart
+service squid restart
+/etc/init.d/nginx restart
+#/etc/init.d/openvpn restart
+rm -rf ~/.bash_history && history -c
+echo "unset HISTFILE" >> /etc/profile
+
+# info
+clear
+echo "Autoscript Include:" | tee log-install.txt
+echo "===========================================" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Service"  | tee -a log-install.txt
+echo "-------"  | tee -a log-install.txt
+echo "OpenSSH   : 22,143"  | tee -a log-install.txt
+echo "Dropbear  : 109,456"  | tee -a log-install.txt
+echo "SSL       : 443"  | tee -a log-install.txt
+echo "Squid3    : 80,8080,3128 (limit to IP SSH)"  | tee -a log-install.txt
+echo "badvpn    : badvpn-udpgw port 7300"  | tee -a log-install.txt
+echo "nginx     : 81"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Script"  | tee -a log-install.txt
+echo "------"  | tee -a log-install.txt
+echo "menu      : Menampilkan daftar perintah yang tersedia"  | tee -a log-install.txt
+echo "usernew   : Membuat Akun SSH"  | tee -a log-install.txt
+echo "trial     : Membuat Akun Trial"  | tee -a log-install.txt
+echo "hapus     : Menghapus Akun SSH"  | tee -a log-install.txt
+echo "cek       : Cek User Login"  | tee -a log-install.txt
+echo "member    : Cek Member SSH"  | tee -a log-install.txt
+echo "jurus69   : Restart Service dropbear, squid3, stunnel4, vpn, ssh)"  | tee -a log-install.txt
+echo "reboot    : Reboot VPS"  | tee -a log-install.txt
+echo "speedtest : Speedtest VPS"  | tee -a log-install.txt
+echo "info      : Menampilkan Informasi Sistem"  | tee -a log-install.txt
+echo "delete    : auto Delete user expired"  | tee -a log-install.txt
+echo "about     : Informasi tentang script auto install"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+
+echo "Fitur lain"  | tee -a log-install.txt
+echo "----------"  | tee -a log-install.txt
+echo "Timezone  : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "IPv6      : [off]"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Modified by hidessh"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Log Instalasi --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "VPS AUTO REBOOT TIAP JAM 12 MALAM"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "==========================================="  | tee -a log-install.txt
+cd
+
+rm -f /root/ovh-ssh.sh
+
+echo "==================== Restart Service ===================="
+echo "========================================================="
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/stunnel4 restart
+/etc/init.d/squid restart
+/etc/init.d/nginx restart
+#/etc/init.d/php5.6-fpm restart
+#/etc/init.d/openvpn restart
+
