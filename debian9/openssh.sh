18b42af467f313604fec1e776f9d22fb80b77344
ID Tunnel <65126630+idtunnel@users.noreply.github.com>
2020-07-26 00:57:22 +0700

    Update openssh.sh

--- debian9/openssh.sh
+++ debian9/openssh.sh
@@ -102,7 +102,7 @@ echo "========================================================="
 # install dropbear
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=44/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 77 "/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
@@ -150,14 +150,14 @@ socket = a:SO_REUSEADDR=1
 socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 [dropbear]
-accept = 440
+accept = 444
 connect = 127.0.0.1:143
 [dropbear]
 accept = 222
 connect = 127.0.0.1:22
 [dropbear]
-accept = 444
-connect = 127.0.0.1:443
+accept = 443
+connect = 127.0.0.1:44
 [dropbear]
 accept = 777
 connect = 127.0.0.1:77
